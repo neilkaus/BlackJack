c53a2031af9361867b31215a43f873d33d231810
andrewwang647 <69985465+andrewwang647@users.noreply.github.com>
2020-08-25 06:15:40 -0400

    Update PlayerGUI.java

--- src/PlayerGUI.java
+++ src/PlayerGUI.java
@@ -304,9 +304,14 @@ public class PlayerGUI extends javax.swing.JFrame {
         txtBetInput.setEditable(true);
         blnSplitStatus = false;
         
+        setDisableAll();
+        
         // resetting the layout
         pnlCards1.setLayout(new FlowLayout());
         pnlCards1.removeAll();
+        pnlCards2.setLayout(new FlowLayout());
+        pnlCards2.removeAll();
+
     }
     
     public void startPlay(){
